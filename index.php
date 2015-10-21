13d16f2a0e3690502bff8b8f0a9614cac8c7c5e2
Alina <ananyivaalina@gmail.com>
2015-10-21 21:59:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,91 +7,129 @@
         <h1>Parser for http://myshows.me/view/187/</h1>
         <p>You can replace http://myshows.me/view/<b>187</b>/ For example http://myshows.me/view/2/ is a Big Bang Theory</p>
 <?php
+/*
+
+Configuration begine. Connecting to the DB.
+
+*/
+$wgDBserver = 'localhost';
+$wgDBuser = 'myUserName';
+$wgDBpassword = 'myPassword';
+$wgDBname = 'myDBName';
+
+$mysqli = new mysqli( $wgDBserver, $wgDBuser, $wgDBpassword );
+
+if ( mysqli_connect_errno() ) {
+    die( "Failed:" . mysqli_connect_error() );
+}
+
+if ( $mysqli->select_db( $wgDBname) ) {
+    echo 'Connected and DB selected.';
+} else {
+    echo 'Something went from (select_db())';
+}
+echo $mysqli->host_info . "\n";
+/*
+
+Configuration. The end.
+
+*/
+
+
+
+/*
+
+Parser begine.
+
+*/
+
 // Include the library
-include('html_dom/simple_html_dom.php');
+include('/home/mangal/data/www/mangal.biz/vizitka/wordpress/wp-admin/html_dom.php');
 
 // Retrieve the DOM from a given URL
-$html = file_get_html('http://myshows.me/view/187/');
-
-//Get usefull data from $html.
+$html = file_get_html('https://afisha.mail.ru/series_756802_vo_vse_tyazhkie/');
 $counter = 0;
-$result  = array(
-    ""
-);
-foreach ($html->find('main.col8>div.clear>p') as $e) { //, main.col8>-div.clear>form
-    if ($counter < 21) { //Line with not usefull content.
-        $counter++;
-        if ($counter > 3) { //Hides lines with not usefull content.
-            $string = strip_tags($e->innertext); //Delete html-attributes.
-            switch ($counter) {
-                case 4:
-                    $publishDates = substr($string, 22) . "<br>";
-                    echo $publishDates;
-                    break;
-                case 5:
-                    $country = substr($string, 14);
-                    echo $country . "<br>";
-                    break;
-                case 6:
-                    $genres = substr($string, 14);
-                    echo $genres . "<br>";
-                    break;
-                case 7:
-                    $channel = substr($string, 11, 4);
-                    echo $channel . "<br>";
-                    break;
-                case 8:
-                    $viewersAmount = substr($string, 19);
-                    echo $viewersAmount . "<br>";
-                    break;
-                case 9:
-                    $generalDuration = substr($string, 37);
-                    echo $generalDuration . "<br>";
-                    break;
-                case 10:
-                    $averageDuration = substr($string, 37);
+foreach ($html->find('.movieabout__info__tab>table>tbody>tr') as $e) { 
+    if($counter < 15){
+	   $counter++;
+	   $string = strip_tags($e->innertext); //Delete html-tags.
+	    if($counter > 3){
+		    switch($counter){//$serialStatus, $directors, $actors, $producers, $screenwriter, $operators, $musician, $premiere, $averageDuration, $channel
+			    case 4:
+				    $serialStatus = substr($string, 12);
+			        echo $serialStatus.'<br>';
+			        break;
+				case 6:
+				    $directors = substr($string, 18);
+				    echo $directors.'<br>';
+			        break;
+				case 7:
+				    $actors = substr($string, 13);
+				    echo $actors.'<br>';
+			        break;
+				case 8:
+				    $producers = substr($string, 18);
+				    echo $producers.'<br>';
+			        break;
+				case 9:
+				    $screenwriter = substr($string, 20);
+				    echo $screenwriter.'<br>';
+			        break;
+				case 10:
+				    $operators = substr($string, 18);
+				    echo $operators.'<br>';
+			        break;
+				case 11: 
+				    $musician = substr($string, 20);
+                    echo $musician.'<br>';
+			        break;				
+				case 12:
+				    $premiere = substr($string, 16);
+				    echo $premiere.'<br>';
+			        break;
+				case 13:
+				    $averageDuration = substr($string, 35);
                     echo $averageDuration . "<br>";
-                    break;
-                case 11:
-                    $ratingIMDB = substr($string, 20);
-                    echo $ratingIMDB . "<br>";
-                    break;
-                case 12:
-                    $ratingKinopoisk = substr($string, 37);
-                    echo $ratingKinopoisk . "<br>";
-                    break;
-                case 13:
-                    $ratingMyShows = substr($string, 185);
-                    echo $ratingMyShows . "<br>";
-                    break;
-                case 16:
-                    $summary = $string;
-                    echo $summary . "<br>";
-                    break;
-                default:
-            }
-        }
-    }
-}
-$episodesAddress = 'main.col8>-div.clear>form.row.col6.widerCont>ul>li>a>span._name';
-$seasonsAddress  = 'main.col8>-div.clear>form.row.col2.widerCont>h4';
-$counter         = 0;
-foreach ($html->find($seasonsAddress) as $season) { //Get all episodes names.
-    $currentSeason = preg_replace("/[^0-9,.]/", "", $season->innertext); //Delete all non-numeric characters.
-    if ($counter == 0) {
-        $counter       = 1;
-        $seasonsAmount = $currentSeason;
-        echo '<h4> Seasons amount is ' . $seasonsAmount . '</h4>';
-    }
-    echo $currentSeason . "<br>"; //Print every episode.
-    //Push every episode in the array.
-    foreach ($html->find($episodesAddress) as $episode) { //Get all episodes names of the current season.
-        echo $episode->innertext . "<br>"; //Print every episode.
-        //Push every episode in the array.
-    }
+			        break;
+				case 15: 
+                    $channel = substr($string, 26);
+                    echo $channel . "<br>";
+			        break;	
+                default:				
+		    }	       
+	    }
+	}
 }
 
-//Put variables in the database.  
+
+
+$result = $mysqli->query("SELECT * FROM afisha WHERE serial_name == 'Breaking Bad';");
+if ($result->num_rows > 0) {//$serialStatus, $directors, $actors, $producers, $screenwriter, $operators, $musician, $premiere, $averageDuration, $channel
+    echoDataFromDB(); //If such row is exist, then update its data.
+} else {
+    echo "Adding data to the db.<br>";
+	$mysqli->query("INSERT INTO afisha (serial_status, directors, actors, producers, screenwriter, operators, musician, premiere, average_duration, channel)
+    VALUES ('$serial_status', '$directors', '$actors', '$producers', '$screenwriter', '$operators', '$musician', '$premiere', '$averageDuration', '$channel');");
+    echoDataFromDB();//Display just created row from the DB.
+}
+$mysqli->close();
+
+function echoDataFromDB(){
+	$res = $mysqli->query("SELECT * FROM afisha ;");
+	while($row = $res->fetch_assoc()) {
+        echo "serialStatus: ".$row["serial_status"]." directors".$row["directors"]
+		."actors: ".$row["actors"]." producers: ".$row["producers"]." screenwriter: ".$row["screenwriter"]
+		." operators: ".$row["operators"]." musician: ".$row["musician"]." channel "." premiere: "
+		.$row["premiere"]." averageDuration: ".$row["averageDuration"].$row["channel"]."<br>";
+	}
+	$mysqli->query("UPDATE afisha SET serial_status = $serialStatus, directors=$directors, actors=$actors, producers=$producers, screenwriter=$screenwriter, operators=$operators, musician=$musician, premiere=$premiere, averageDuration=$average_duration, channel=$channel");
+}
+/*
+
+Parser. The end..
+
+*/
+
 ?>
    </body>
 </html>
